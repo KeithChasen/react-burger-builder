7ce8f46f7e6db8e00ee3e89d0afb8de11ac8629c
keithchasen <keithchasen89@gmail.com>
2019-10-26 10:24:15 +0300

    lifecycle hooks for optimizing order summury inside modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(key => {
-             return <li key={key}>
+class OrderSummary extends Component {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Order Summary] did update')
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(key => {
+                return <li key={key}>
                         <span style={{ textTransform: 'capitalize'}}>
                             {key}
                         </span>:
-                            {props.ingredients[key]}
-                     </li>
-        })
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    )
+                    {this.props.ingredients[key]}
+                </li>
+            })
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Aux>
+        )
+    }
+
 }
 
-export default orderSummary
+export default OrderSummary
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Modal.module.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = props => (
-    <Aux>
-        <Backdrop
-            show={props.show}
-            clicked={props.modalClosed}
-        />
-        <div className={classes.Modal} style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
 
-export default modal
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        //Modal wraps Order summary
+        //so in case Order summary is not shown
+        // not to let it be rerendered
+        // any time some state value changed such as price or ingredient
+        //we need to check its show property
+        // so basically in this case:
+        // currently show is FALSE and next show is true
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Modal] DidUpdate')
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+                <div className={classes.Modal} style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+
+
+export default Modal
