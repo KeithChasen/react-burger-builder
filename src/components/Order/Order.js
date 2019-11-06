8ebbb63ef1f21fb0b399139df62ad1f6225fee4c
keithchasen <keithchasen89@gmail.com>
2019-11-06 17:48:51 +0200

    outputting orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,12 +1,39 @@
 import React from 'react'
 
 import classes from './Order.module.css'
+import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
-const order = props => (
-    <div className={classes.Order}>
-        <p>Ingredients: </p>
-        <p>Price: <strong>{ props.price }</strong></p>
-    </div>
-)
+const order = props => {
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName, amount: props.ingredients[ingredientName]
+        })
+    }
+
+    const ingredientOutput = ingredients.map(ingredient => {
+        return <span
+            key={ingredient.name}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '8px'
+            }}
+        >
+            {ingredient.name} ({ingredient.amount})
+        </span>
+    })
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: </p>
+            { ingredientOutput }
+            <p>Price: <strong>$ { Number.parseFloat(props.price).toFixed(2) }</strong></p>
+        </div>
+    )
+}
 
 export default order
