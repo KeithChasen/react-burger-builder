bde80ac792891786fb719b9ae5aac8ec029fc11e
keithchasen <keithchasen89@gmail.com>
2019-11-11 13:05:46 +0200

    sending order to server with new form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -70,9 +70,16 @@ class ContactData extends Component {
             loading: true
         })
 
+        const formData = {}
+
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+        }
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price.toFixed(2),
+            orderData: formData
         }
 
         // using firebase, so need to fire <path>.json
@@ -122,7 +129,7 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 { formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
@@ -132,7 +139,7 @@ class ContactData extends Component {
                         changed={(event) => this.inputChanged(event, formElement.id)}
                     />
                 )) }
-                <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+                <Button btnType="Success">Order</Button>
             </form>
         )
         if (this.state.loading) {
