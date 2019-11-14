ed29f9ce36c831e26eb000f303f3a36c79d2d528
keithchasen <keithchasen89@gmail.com>
2019-11-14 18:49:06 +0200

    updated contact data and checkout with redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders'
+import { connect } from 'react-redux'
 
 class ContactData extends Component {
     state = {
@@ -106,7 +107,7 @@ class ContactData extends Component {
         }
 
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price.toFixed(2),
             orderData: formData
         }
@@ -222,4 +223,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData)
