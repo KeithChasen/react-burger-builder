fa795de357995a1ae68b7e5002bab48cd0fcb905
keithchasen <keithchasen89@gmail.com>
2020-01-02 18:32:19 +0200

    using updateObject utility in Auth and ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import axios from '../../../axios-orders'
 import { connect } from 'react-redux'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { updateObject } from "../../../shared/utility";
 
 class ContactData extends Component {
     state = {
@@ -146,22 +147,18 @@ class ContactData extends Component {
     }
 
     inputChanged = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        }
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid: this.checkValidity(
+                event.target.value,
+                this.state.orderForm[inputIdentifier].validation
+            ),
+            touched: true
+        })
 
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        }
-
-        updatedFormElement.value = event.target.value
-
-        updatedFormElement.valid = this.checkValidity(
-            updatedFormElement.value,
-            updatedFormElement.validation
-        )
-
-        updatedFormElement.touched = true
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        })
 
         updatedOrderForm[inputIdentifier] = updatedFormElement
 
