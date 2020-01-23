f42c0cde7c8f34662340baedc1f546283b8d0d20
keithchasen <keithchasen89@gmail.com>
2020-01-23 18:33:07 +0200

    using takeLatest effect to prevent excuting all the previous actions

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery, all } from 'redux-saga/effects'
+import { takeEvery, all, takeLatest } from 'redux-saga/effects'
 import * as actionTypes from '../actions/actionTypes'
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth'
 import { initIngredientsSaga } from "./burgerBuilder";
@@ -21,6 +21,6 @@ export function* watchBurgerBuilder() {
 }
 
 export function* watchOrderBuilder() {
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
     yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga)
 }
